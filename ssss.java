54c7f41641dd58b0050c1262c4d367a7a335d9ed
sahanal2603 <53811265+sahanal2603@users.noreply.github.com>
2019-08-24 13:01:30 +0530

    Add files via upload

---
+++ ssss.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+ class abc
+ {
+  public void greet()
+  {
+   System.out.println("Hello,good morning");
+  }
+  public void greet2()
+  {
+   System.out.println("Good evening");
+  }
+
+ }
+ class a
+ {
+  public static void main(String args[])
+  {
+   abc a1=new abc();
+   a1.greet();
+   a1.greet2();
+  }
+}
+
